74781c09c47a299d491ae737cb240bf63d053483
Ahmed Ayinla <aayprince@gmail.com>
2025-09-14 11:57:21 +0100

    feat: add global styling

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -4,6 +4,7 @@ import { useNavigate } from 'react-router-dom';
 import { Eye, EyeOff, Loader2 } from 'lucide-react';
 import { loginUser } from '../utils/mockApi';
 import { useToast } from '../use-toast';
+import './Login.scss';
 
 const Login: React.FC = () => {
   const [email, setEmail] = useState('');
