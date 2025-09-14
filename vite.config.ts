b117a752717331e4ead0d307e7ffa6ca3c90a486
Ahmed Ayinla <aayprince@gmail.com>
2025-09-14 12:13:00 +0100

    removed redundant sstyling files

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,21 @@
 // vite.config.ts
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
-
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // ✅ Use the alias we defined so Vite can resolve it
+        additionalData: `@import "@/styles/globals.scss";`,
+      },
     },
   },
 })
