937c47563b04078a43644d67f0515922f1efbf88
Ahmed Ayinla <aayprince@gmail.com>
2025-09-17 08:17:08 +0100

    removed js duplicate file

--- src/components/ui/Input.js
+++
@@ -1,9 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-// src/components/ui/Input.tsx
-import * as React from "react";
-import classNames from "classnames";
-import "./Input.scss";
-export const Input = React.forwardRef(({ className, ...props }, ref) => {
-    return _jsx("input", { ref: ref, className: classNames("input", className), ...props });
-});
-Input.displayName = "Input";
